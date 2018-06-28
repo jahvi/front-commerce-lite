20ed9e75b5d52915a1e38bebd7eb84ae7214ff87
Sylvain Boulade <sylvain@occitech.fr>
2018-06-28 16:50:27 +0200

    Add necessary icons for Reinsurance

--- src/web/theme/ui/atoms/Icon/Icon.js
+++ src/web/theme/ui/atoms/Icon/Icon.js
@@ -4,6 +4,9 @@ import arrowLeft from "./arrow-left.svg?inline";
 import close from "./close.svg?inline";
 import trash from "./trash.svg?inline";
 import cart from "./cart.svg?inline";
+import lock from "./lock.svg?inline";
+import time from "./time.svg?inline";
+import like from "./like.svg?inline";
 import "./Icon.scss";
 
 const Icon = ({ icon }) => {
@@ -40,13 +43,45 @@ const Icon = ({ icon }) => {
         aria-hidden={true}
       />
     );
+  } else if (icon === "lock") {
+    return (
+      <span
+        className="icon"
+        dangerouslySetInnerHTML={{ __html: lock }}
+        aria-hidden={true}
+      />
+    );
+  } else if (icon === "time") {
+    return (
+      <span
+        className="icon"
+        dangerouslySetInnerHTML={{ __html: time }}
+        aria-hidden={true}
+      />
+    );
+  } else if (icon === "like") {
+    return (
+      <span
+        className="icon"
+        dangerouslySetInnerHTML={{ __html: like }}
+        aria-hidden={true}
+      />
+    );
   }
 
   return icon;
 };
 
 Icon.propTypes = {
-  icon: PropTypes.oneOf(["arrow-left", "close", "trash", "cart"]).isRequired
+  icon: PropTypes.oneOf([
+    "arrow-left",
+    "close",
+    "trash",
+    "cart",
+    "lock",
+    "time",
+    "like"
+  ]).isRequired
 };
 
 export default Icon;
